f6ce258f20d31b95f84678ba07f663329c3d571b
Aakash Goel <aakash.lpin@gmail.com>
2016-08-31 15:37:03 +0530

    updated to grab name, price and image

--- index_nightmare.js
+++ index_nightmare.js
@@ -1,17 +1,22 @@
 // var app = require('express')();
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: false })
+var nightmare = Nightmare({ show: false });
 var url = 'https://dl.flipkart.com/dl/levi-s-two-ply-slippers/p/itmefrubpuavgh8y?pid=SFFEFRUBRWYUZMSK';
 
 nightmare
   .useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36")
   .viewport(1280, 768)
   .goto(url)
-  .wait(10000)
+  .wait('._1vC4OE._37U4_g')
   .evaluate(function () {
-    // return document.querySelector('body').innerHTML;
-    // return document.querySelector('.pricing .selling-price').innerHTML;
-    return document.querySelector('._1vC4OE._37U4_g').innerHTML;
+    var image = document.querySelector('.sfescn').getAttribute('src');
+    var price = document.querySelector('._1vC4OE._37U4_g').innerHTML;
+    var name = document.querySelector('._3eAQiD').innerHTML;
+    return {
+      image: image,
+      price: price,
+      name: name,
+    }
   })
   .end()
   .then(function (result) {
