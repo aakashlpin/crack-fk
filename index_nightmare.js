57d785a16dfbc3f0e3004612e2f38a46f4571039
Aakash Goel <aakash.lpin@gmail.com>
2016-08-31 16:23:18 +0530

    updated url

--- index_nightmare.js
+++ index_nightmare.js
@@ -1,6 +1,6 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: false })
-var url = 'https://dl.flipkart.com/dl/levi-s-two-ply-slippers/p/itmefrubpuavgh8y?pid=SFFEFRUBRWYUZMSK';
+var url = 'https://www.flipkart.com/fitbit-blaze-black-silver-smartwatch/p/itmejxmuyhfjzcgq';
 
 nightmare
   .useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36")
