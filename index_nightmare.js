e2f82ed71230dc7265914a4db3340ce063b16ac3
Aakash Goel <aakash.lpin@gmail.com>
2016-08-31 15:46:32 +0530

    what is happening?

--- index_nightmare.js
+++ index_nightmare.js
@@ -7,15 +7,15 @@ nightmare
   .useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36")
   .viewport(1280, 768)
   .goto(url)
-  .wait('._1vC4OE._37U4_g')
+  .wait(6000)
   .evaluate(function () {
-    var image = document.querySelector('.sfescn').getAttribute('src');
     var price = document.querySelector('._1vC4OE._37U4_g').innerHTML;
-    var name = document.querySelector('._3eAQiD').innerHTML;
+    // var image = document.querySelector('.sfescn').getAttribute('src');
+    // var name = document.querySelector('._3eAQiD').innerHTML;
     return {
-      image: image,
+      // image: image,
       price: price,
-      name: name,
+      // name: name,
     }
   })
   .end()
@@ -34,6 +34,9 @@ nightmare
 // app.listen(process.env.PORT || 3000);
 
 
+process.on('uncaughtException', function (err) {
+  console.log(err);
+})
 
 // var selector = '[data-reactid="107"] > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(1)';
 // var selector123 = ;
