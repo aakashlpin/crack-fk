29526c6633220a4863745d7e11e225ed0a79ae12
Aakash Goel <aakash.lpin@gmail.com>
2016-10-30 13:12:06 +0530

    added shell commands to ensure Xvfb is running before initiating electron

--- engine/reporter.js
+++ engine/reporter.js
@@ -1,7 +1,11 @@
 var Nightmare = require('nightmare');
+var shell = require('shelljs');
 var url = 'https://www.amazon.in/ap/signin?openid.return_to=https%3A%2F%2Faffiliate-program.amazon.in%2F&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_associates_in&openid.mode=checkid_setup&marketPlaceId=A21TJRUUN4KGV&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.pape.max_auth_age=0';
 
 function reporter () {
+  shell.exec('export DISPLAY=:99');
+  shell.exec('Xvfb :99 &', { silent: true });
+
   var nightmare = Nightmare({ show: true });
 
   var time = new Date();
