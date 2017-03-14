191bf40bedb684b3362f493d7fe6d160a2af88dd
Aakash Goel <aakash.lpin@gmail.com>
2017-03-14 18:24:59 +0530

    yeh bhi le lo

--- shipitfile.js
+++ shipitfile.js
@@ -20,7 +20,7 @@ module.exports = function (shipit) {
 
   shipit.task('start', function () {
     return shipit.remote('cd /tmp/deploy_to/current && npm i --verbose').then(function () {
-      shipit.remote('cd /tmp/deploy_to/current && npm run start').then(function () {
+      shipit.remote('cd /tmp/deploy_to/current && mkdir logs && npm run start').then(function () {
         shipit.log('all done');
       })
     })
