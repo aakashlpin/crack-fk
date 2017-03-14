ec7baa38f35d6c24eb79b85fc724665c18836b72
Aakash Goel <aakash.lpin@gmail.com>
2017-03-14 17:43:56 +0530

    deploy this shiz

---
+++ shipitfile.js
@@ -0,0 +1,28 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/tmp/deploy_to',
+      branch: 'master',
+      repositoryUrl: 'https://github.com/aakashlpin/crack-fk.git',
+      ignores: ['.git', 'node_modules'],
+      rsync: ['--del'],
+      keepReleases: 2,
+      key: '/Users/aakashlpin/.ssh/id_rsa',
+      shallowClone: true
+    },
+    production: {
+      servers: 'aakashlpin@flipkart.cheapass.in'
+    },
+  });
+
+  shipit.task('start', function () {
+    return shipit.remote('cd /tmp/deploy_to/current && npm i').then(function () {
+      return shipit.remote('cd /tmp/deploy_to/current && npm run start').then(function () {
+        shipit.log('all done');
+      })
+    })
+  })
+}
